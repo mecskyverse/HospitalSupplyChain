d2debfc318b2e9d5ccec6adbde7081f59930abd7
Aakash <vermaaakash1000@gmail.com>
2023-11-27 18:25:17 +0530

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Monteserrat:['Montserrat', 'sans'],
+        Cabin: ['Cabin', 'sans'],
+      }
+    },
+  },
+  plugins: [],
+}
+
