7c7777f0a6049f8ac3d32653fc5e2e3d94112ce8
Aakash <vermaaakash1000@gmail.com>
2023-11-27 18:28:03 +0530

    Dashboard Initialized

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SearchField from '../components/SearchField.jsx'
+function Dashboard() {
+  return (
+    <div>
+      <SearchField />
+    </div>
+  )
+}
+
+export default Dashboard
