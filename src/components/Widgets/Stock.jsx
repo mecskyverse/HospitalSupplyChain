614b04237d553bf571b3da28ba9332f393153bbd
Aakash <vermaaakash1000@gmail.com>
2023-11-29 04:30:22 +0530

    Stocks Widget Updated

--- src/components/Widgets/Stock.jsx
+++ src/components/Widgets/Stock.jsx
@@ -24,9 +24,9 @@ const OHLC = () => {
     };
 
     return (
-        <div className='bg-[#342f45] ' >
+        <div className='bg-[#342f45] rounded-lg ' >
             <style>{SAMPLE_CSS}</style>
-            <StockChartComponent width='700px' height='342px'  primaryXAxis={{ valueType: 'DateTime', majorGridLines: { width: 0 }, majorTickLines: { color: 'transparent' }, crosshairTooltip: { enable: true } }} primaryYAxis={{ labelFormat: 'n0', lineStyle: { width: 0 }, rangePadding: 'None', majorTickLines: { height: 0 } }} chartArea={{ border: { width: 0 } }} tooltip={{ enable: true, shared: true, position: 'Nearest' }} tooltipRender={tooltipRender} crosshair={{ enable: true }} title='Monthly Revenue Progress' titleStyle={titlestyle} theme='MaterialDark'>
+            <StockChartComponent width='600px' height='342px' border={{ color: 'red', width: 0 }} primaryXAxis={{ valueType: 'DateTime', majorGridLines: { width: 0 }, majorTickLines: { color: 'transparent' }, crosshairTooltip: { enable: true } }} primaryYAxis={{ labelFormat: 'n0', lineStyle: { width: 0 }, rangePadding: 'None', majorTickLines: { height: 0 } }} chartArea={{ border: { width: 0 } }} tooltip={{ enable: true, shared: true, position: 'Nearest' }} tooltipRender={tooltipRender} crosshair={{ enable: true }} title='Monthly Revenue Progress' titleStyle={titlestyle} theme='MaterialDark'>
                 <Inject services={[DateTime, Tooltip, RangeTooltip, Crosshair, LineSeries, SplineSeries, CandleSeries, HiloOpenCloseSeries, HiloSeries, RangeAreaSeries, Trendlines, EmaIndicator, RsiIndicator, BollingerBands, TmaIndicator, MomentumIndicator, SmaIndicator, AtrIndicator, Export, AccumulationDistributionIndicator, MacdIndicator, StochasticIndicator]} />
                 <StockChartSeriesCollectionDirective>
                     <StockChartSeriesDirective dataSource={stockData1} xName='x' high='high' low='low' open='open' close='close' type='HiloOpenClose' bearFillColor={'#9D7FFE'} bullFillColor={'#39D3EC'} animation={{ enable: true }} />
