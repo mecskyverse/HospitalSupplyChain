c80eb682ec785d2ff1b732a335ecaaecab984aa8
Aakash <vermaaakash1000@gmail.com>
2023-11-29 04:31:04 +0530

    Additional Widgets added

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,10 +3,13 @@ import SearchField from '../components/SearchField.jsx'
 import Calender from '../components/Widgets/Calender.jsx'
 import DailyOverview from '../components/Widgets/DailyOverview.jsx'
 import Stock from '../components/Widgets/Stock.jsx'
+import group5 from '../assets/Dashboard/group5.svg'
+import IncomeState from '../components/Widgets/IncomeState.jsx'
+import SpendingStatistics from '../components/Widgets/SpendingStatistics.jsx'
 function Dashboard() {
   const Dailydata = ['Visitors', 'Customers', 'Orders', 'Shipment']
   return (
-    <div>
+    <div className=''>
       <SearchField />
       <div className='p-5 flex justify-center flex-col items-center'>
         <div className='flex gap-5'>{Dailydata.map((data, index) => {
@@ -15,9 +18,13 @@ function Dashboard() {
           )
         })}
         </div>
-        <div className='w-full flex justify-around pt-4 gap-2'>
+        <div className='w-full justify-center gap-3 flex mt-3 items-start'>
           <Stock />
           <Calender />
+          <div className='flex flex-col gap-2'>
+            <IncomeState />
+            <SpendingStatistics />
+          </div>
         </div>
       </div>
 
