15ca5eb47b6c65bd5ba249d4ddeaf31f55c0e616
Aakash <vermaaakash1000@gmail.com>
2023-11-29 00:24:10 +0530

    Stock File Updated

---
+++ src/components/Widgets/Stock.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { useEffect } from "react";
+import { StockChartComponent, StockChartSeriesCollectionDirective, StockChartSeriesDirective, Inject, DateTime, Tooltip, RangeTooltip, Crosshair, LineSeries, SplineSeries, CandleSeries, HiloOpenCloseSeries, HiloSeries, RangeAreaSeries, Trendlines } from '@syncfusion/ej2-react-charts';
+import { EmaIndicator, RsiIndicator, BollingerBands, TmaIndicator, MomentumIndicator, SmaIndicator, AtrIndicator, AccumulationDistributionIndicator, MacdIndicator, StochasticIndicator, Export } from '@syncfusion/ej2-react-charts';
+import stockData1 from '../stockData';
+
+const SAMPLE_CSS = `
+    .control-fluid {
+        padding: 0px !important;
+    }
+    .charts {
+        align :center;
+    }`;
+const titlestyle = { fontFamily: "Arial", color: '#ffffff', textOverflow: 'Wrap' };
+const OHLC = () => {
+    const tooltipRender = (args) => {
+        if (args.text.split('<br/>')[4]) {
+            let target = parseInt(args.text.split('<br/>')[4].split('<b>')[1].split('</b>')[0]);
+            let value = (target / 100000000).toFixed(1) + 'B';
+            args.text = args.text.replace(args.text.split('<br/>')[4].split('<b>')[1].split('</b>')[0], value);
+        }
+    };
+
+    return (
+        <div className='bg-[#342f45] '>
+            {/* <style>{SAMPLE_CSS}</style> */}
+            <StockChartComponent primaryXAxis={{ valueType: 'DateTime', majorGridLines: { width: 0 }, majorTickLines: { color: 'transparent' }, crosshairTooltip: { enable: true } }} primaryYAxis={{ labelFormat: 'n0', lineStyle: { width: 0 }, rangePadding: 'None', majorTickLines: { height: 0 } }} chartArea={{ border: { width: 0 } }} tooltip={{ enable: true, shared: true, position: 'Nearest' }} tooltipRender={tooltipRender} crosshair={{ enable: true }} title='Monthly Revenue Progress' titleStyle={titlestyle} >
+                <Inject services={[DateTime, Tooltip, RangeTooltip, Crosshair, LineSeries, SplineSeries, CandleSeries, HiloOpenCloseSeries, HiloSeries, RangeAreaSeries, Trendlines, EmaIndicator, RsiIndicator, BollingerBands, TmaIndicator, MomentumIndicator, SmaIndicator, AtrIndicator, Export, AccumulationDistributionIndicator, MacdIndicator, StochasticIndicator]} />
+                <StockChartSeriesCollectionDirective>
+                    <StockChartSeriesDirective dataSource={stockData1} xName='x' high='high' low='low' open='open' close='close' type='HiloOpenClose' bearFillColor={'#39D3EC'} bullFillColor={'#9D7FFE'} animation={{ enable: true }} />
+                </StockChartSeriesCollectionDirective>
+            </StockChartComponent>
+        </div>
+    );
+};
+export default OHLC;
