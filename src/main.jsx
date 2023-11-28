232e340875674a89d5217fc6775c8b02cdd98bfd
Aakash <vermaaakash1000@gmail.com>
2023-11-28 23:09:55 +0530

    syncfusion integration added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerLicense } from '@syncfusion/ej2-base';
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0Qnxbf1xzZF1MZV1bQX9PMyBoS35RdURiW3heeHFXRmBZV0Rw');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
