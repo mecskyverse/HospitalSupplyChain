c4387d2b892781af5b7deea407eb09d771f0a698
Aakash <vermaaakash1000@gmail.com>
2023-11-28 23:11:52 +0530

    Dashboard Updated

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import SearchField from '../components/SearchField.jsx'
+import Calender from '../components/Widgets/Calender.jsx'
+import DailyOverview from '../components/Widgets/DailyOverview.jsx'
 function Dashboard() {
+  const Dailydata = ['Visitors', 'Customers', 'Orders', 'Shipment']
   return (
     <div>
       <SearchField />
+      <div className='p-5 flex justify-center flex-col items-center'>
+        <div className='flex gap-5'>{Dailydata.map((data, index) => {
+          return (
+            <DailyOverview currData={data} index={index} />
+          )
+        })}</div>
+        <Calender />
+      </div>
+
     </div>
   )
 }
