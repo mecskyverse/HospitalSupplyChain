69ab1cf4b81b13e0411cbc6461601b9d1bc210a5
Aakash <vermaaakash1000@gmail.com>
2023-11-29 06:01:19 +0530

    Navbar Active Styles added

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -13,12 +13,22 @@ import vector8 from '../assets/Dashboard/vector8.svg'
 function Navbar() {
   const navItems = [vector1, vector2, vector3, vector4, vector5, vector6, vector7, vector8]
   const mouseOverText = ['Analytics', 'Supply Chain Track', 'Inventory Management', 'Transaction Management', 'Regulatory Compliance', 'User Management', 'Product Registration', 'Product Verification']
+  const activeStyle = {
+    backgroundColor: 'grey',
+    backdropFilter: 'blur(10px)',
+    width: '5vw',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center'
+    // backdropFilter: 'blur(8px)'
+  }
+  const routes = ['', 'supplyChainTrack', 'inventoryManagement', 'transactionManagement', 'regulatoryCompliance', 'userManagement', 'productRegistration', 'productVerification']
   return (
     <div className='h-screen bg-[#7743DB] w-[5vw] sticky top-0'>
       <nav className='flex flex-col h-full gap-8 relative pt-24 items-center'>
         <div className='flex text-2xl items-center absolute top-4 '> <FaLink /><span className='font-bold font-Cabin'>HC</span></div>
         {navItems.map((item, index) => {
-          return (<button title={mouseOverText[index]} key={index} className='transition ease-in-out delay-150 duration-200 hover:scale-110'><img src={item} alt='navlink' /></button>)
+          return (<NavLink style={({ isActive }) => isActive ? activeStyle : null} to={routes[index]} title={mouseOverText[index]} key={index} className='transition ease-in-out delay-150 duration-200 hover:scale-110'><img src={item} alt='navlink' /></NavLink>)
         })}
       </nav>
     </div>
