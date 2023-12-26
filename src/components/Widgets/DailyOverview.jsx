cc6fadf53775be593ca65c7dcb47f559cab352aa
Aakash <vermaaakash1000@gmail.com>
2023-12-26 22:12:10 +0530

    Daily Overview Component Updated

--- src/components/Widgets/DailyOverview.jsx
+++ src/components/Widgets/DailyOverview.jsx
@@ -3,17 +3,17 @@ import group1 from '../../assets/DailyOverview/group1.svg'
 import group2 from '../../assets/DailyOverview/group2.svg'
 import group3 from '../../assets/DailyOverview/group3.svg'
 import group4 from '../../assets/DailyOverview/group4.svg'
-function DailyOverview({ currData, index }) {
+function DailyOverview({ currData, index, dailyValues, transactionManagement }) {
     // Array of image sources
     const imageSources = [group1, group2, group3, group4];
-    const dailyValues = ['721,882', '22,370', '10.604', '50.839']
     const dailyRatio = ['-4.66%', '+0.45%', '-1.07%', '+0.66%']
+    console.log(transactionManagement)
     return (
-        <div className='w-[300px] h-[110px] bg-white rounded-2xl relative'>
+        <div className='w-[300px] h-[110px] bg-white rounded-2xl relative '>
             <div className='text-black flex justify-between'>
-                <div className='text-black font-Cabin text-xl pl-4 pt-2'>Daily {currData}</div>
+                <div className='text-black font-Cabin text-xl pl-4 pt-2'>{currData}</div>
                 <span className='flex flex-col pr-4'>
-                    <div className='pt-1 text-lg'>{dailyValues[index]}</div>
+                    <div className='pt-1 text-lg'>{transactionManagement ? <>&#165;</> : ''} {dailyValues[index]}</div>
                     <span className={`${dailyRatio[index].charAt(0) === '+' ? 'text-green-700' : 'text-red-700'} text-sm self-end`}>{dailyRatio[index]}</span>
                 </span>
             </div>
