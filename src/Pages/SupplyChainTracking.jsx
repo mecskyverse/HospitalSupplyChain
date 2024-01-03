446127b38bc548fc7deb7cbf14198c2bd46d0232
Aakash <vermaaakash1000@gmail.com>
2024-01-03 18:14:00 +0530

    Tracking bar imported

--- src/Pages/SupplyChainTracking.jsx
+++ src/Pages/SupplyChainTracking.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SearchField from '../components/SearchField';
-
+import TrackingBar from '../components/TrackingBar';
 function SupplyChainTracking() {
     return (
-        <div>
+        <div className='flex flex-col items-center'>
             <SearchField />
-            <h1 className='text-4xl text-white text-center'>Supply Chain tracking</h1>
+            <h1 className='text-4xl text-white text-center mt-3'>Supply Chain tracking</h1>
+            <TrackingBar />
         </div>
     )
 }
