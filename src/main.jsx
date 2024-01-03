1008dfe748da47c4f71904d705e085704e084be1
Aakash <vermaaakash1000@gmail.com>
2024-01-03 18:13:34 +0530

    license key updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0Qnxbf1xzZF1MZV1bQX9PMyBoS35RdURiW3heeHFXRmBZV0Rw');
+registerLicense('ORg4AjUWIQA/Gnt2UVhhQlVFfV5dXmJWfFN0QXNfdV15flFHcC0sT3RfQFljSH5SdkNmXXxbcH1dRQ==');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
